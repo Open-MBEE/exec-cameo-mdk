fd12df5fc354bd8fee96c0f613abfc4e8e0a0511
Alek Kerzhner <Aleksandr.A.Kerzhner@jpl.nasa.gov>
2013-09-17 09:33:19 -0700

    Updates to MEL Tables to allow selection of Characterization used, other small improvements/bug fixes

--- src/main/java/gov/nasa/jpl/mgss/mbee/docgen/model/DeploymentTable.java
+++ src/main/java/gov/nasa/jpl/mgss/mbee/docgen/model/DeploymentTable.java
@@ -14,44 +14,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class;
-import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 
 
 
 public class DeploymentTable extends WorkpackageTable {
-	
+
 	public WorkpackageRollups getRollup(boolean fix) {
-		Class c = null;
-		for (Element t: this.targets) {
-			if (!(t instanceof Class)) {
-				continue;
-			}
-			c = (Class)t;
-			break;
-		}
-		if (c == null)
+		findCharacterizationName();		
+		Class _class = findFirstClass();
+
+		if (_class == null)
 			return null;
-		Deployment d = new Deployment(c, this.floatingPrecision, sortByName, suppliesAsso, authorizesAsso, includeInherited);
-		d.getDeploymentTable();
-		return getRollup(d, c, fix, true, null);
+
+		Deployment deployment = new Deployment(_class, this.floatingPrecision, sortByName, suppliesAsso, authorizesAsso, includeInherited);
+		deployment.getDeploymentTable();
+		return getRollup(deployment, _class, fix, true, null);
 	}
-	
+
 	public EditableTable getEditableTable() {
-		Class c = null;
-		for (Element t: this.targets) {
-			if (!(t instanceof Class)) {
-				continue;
-			}
-			c = (Class)t;
-			break;
-		}
-		if (c == null)
+		findCharacterizationName();		
+
+		Class _class = findFirstClass();
+
+		if (_class == null)
 			return null;
-		Deployment d = new Deployment(c, this.floatingPrecision, sortByName, suppliesAsso, authorizesAsso, includeInherited);
+
+		Deployment d = new Deployment(_class, this.floatingPrecision, sortByName, suppliesAsso, authorizesAsso, includeInherited);
 		d.getDeploymentTable();
 		return d.getDeploymentEditableTable();
 	}
-	
+
 	public WorkpackageRollups getRollup(Deployment d, Class c, boolean fix, boolean gui, List<List<DocumentElement>> body) {
 		WorkpackageRollups roll = new WorkpackageRollups(d.getDeployment(), null, null, 
 				d.getRealUnits(), null,
@@ -65,64 +57,61 @@ public class DeploymentTable extends WorkpackageTable {
 		roll.validateOrFix(c, body);
 		return roll;
 	}
-	
+
 	@Override
 	public void visit(boolean forViewEditor, DBHasContent parent, String outputDir) {
+		findCharacterizationName();		
+
 		int i = 0;
-	    List< Element > targets =
-	        isSortElementsByName() ? Utils.sortByName( getTargets() )
-	                                 : getTargets();
-	    for (Element t: targets) {
-				if (!(t instanceof Class)) {
-					continue;
-				}
-				Deployment d = new Deployment((Class)t, getFloatingPrecision(), isSortByName(), isSuppliesAsso(), isAuthorizesAsso(), isIncludeInherited());
-				d.getDeploymentTable();
-				List<List<DocumentElement>> body = new ArrayList<List<DocumentElement>>();
-				WorkpackageRollups roll = getRollup(d, (Class)t, false, false, body);
-				if (roll.isBad()) {
-					parent.addElement(new DBParagraph("<emphasis role=\"bold\">The deployment for " + ((Class)t).getName() + " did not pass mass rollup validation!</emphasis>"));
-					DBTable validation = new DBTable();
-					validation.setBody(body);
-					validation.setTitle("Mass Rollup Validation for Deployment Table - " + ((Class)t).getName());
-					List<List<DocumentElement>> header = new ArrayList<List<DocumentElement>>();
-					List<DocumentElement> headerline = new ArrayList<DocumentElement>();
-					headerline.add(new DBText("Element"));
-					headerline.add(new DBText("Property"));
-					headerline.add(new DBText("Calculated"));
-					headerline.add(new DBText("Model"));
-					header.add(headerline);
-					validation.setHeaders(header);
-					validation.setCols(4);
-					parent.addElement(validation);
-				} 
-				if (!roll.isBad() || forViewEditor) {
-				
-					EditableTable et = d.getDeploymentEditableTable();
-					int depth = d.getProductDepth();
-					DBTable dta = null;
-					if (!forViewEditor)
-						dta = Utils.getDBTableFromEditableTable(et, true, depth);
-					else
-						dta = Utils.getDBTableFromEditableTable(et, true);
-					String title = et.getTitle();
-					if (getTitles() != null && getTitles().size() > i)
-						title = getTitles().get(i);
-					title = getTitlePrefix() + title + getTitleSuffix();
-					dta.setTitle(title);
-					if (getCaptions() != null && getCaptions().size() > i && isShowCaptions())
-						dta.setCaption(getCaptions().get(i));
-					parent.addElement(dta);
-					dta.setStyle(getStyle());
-				}
-				i++;
-			}
+		Class _class = findFirstClass();
+
+		Deployment deployment = new Deployment((Class)_class, getFloatingPrecision(), isSortByName(), isSuppliesAsso(), isAuthorizesAsso(), isIncludeInherited());
+		deployment.getDeploymentTable();
+		List<List<DocumentElement>> body = new ArrayList<List<DocumentElement>>();
+		WorkpackageRollups roll = getRollup(deployment, (Class)_class, false, false, body);
+		if (roll.isBad()) {
+			parent.addElement(new DBParagraph("<emphasis role=\"bold\">The deployment for " + ((Class)_class).getName() + " did not pass mass rollup validation!</emphasis>"));
+			DBTable validation = new DBTable();
+			validation.setBody(body);
+			validation.setTitle("Mass Rollup Validation for Deployment Table - " + ((Class)_class).getName());
+			List<List<DocumentElement>> header = new ArrayList<List<DocumentElement>>();
+			List<DocumentElement> headerline = new ArrayList<DocumentElement>();
+			headerline.add(new DBText("Element"));
+			headerline.add(new DBText("Property"));
+			headerline.add(new DBText("Calculated"));
+			headerline.add(new DBText("Model"));
+			header.add(headerline);
+			validation.setHeaders(header);
+			validation.setCols(4);
+			parent.addElement(validation);
+		} 
+		if (!roll.isBad() || forViewEditor) {
+
+			EditableTable et = deployment.getDeploymentEditableTable();
+			int depth = deployment.getProductDepth();
+			DBTable dta = null;
+			if (!forViewEditor)
+				dta = Utils.getDBTableFromEditableTable(et, true, depth);
+			else
+				dta = Utils.getDBTableFromEditableTable(et, true);
+			String title = et.getTitle();
+			if (getTitles() != null && getTitles().size() > i)
+				title = getTitles().get(i);
+			title = getTitlePrefix() + title + getTitleSuffix();
+			dta.setTitle(title);
+			if (getCaptions() != null && getCaptions().size() > i && isShowCaptions())
+				dta.setCaption(getCaptions().get(i));
+			parent.addElement(dta);
+			dta.setStyle(getStyle());
+		}
+		i++;
 	}
-	
+
+
 	@Override
 	public void accept(IModelVisitor v) {
 		v.visit(this);
-		
+
 	}
 
 }
