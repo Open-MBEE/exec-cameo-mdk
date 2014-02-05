1f9d3ec3a0fced6c8c5832f4d24fb657596ff72f
Bradley J Clement <Bradley.J.Clement@jpl.nasa.gov>
2014-02-05 15:32:05 -0800

    allowing ValueSpecifications to be exported like other elements

--- src/main/java/gov/nasa/jpl/mbee/ems/ModelExporter.java
+++ src/main/java/gov/nasa/jpl/mbee/ems/ModelExporter.java
@@ -102,7 +102,8 @@ public class ModelExporter {
     private boolean addToElements(Element e, int curdepth) {
         if (elements.containsKey(e.getID()))
             return true;
-        if (e instanceof ValueSpecification || !(e instanceof Package) && packageOnly || e instanceof Extension || e instanceof ProfileApplication)
+        if (//e instanceof ValueSpecification || 
+            !(e instanceof Package) && packageOnly || e instanceof Extension || e instanceof ProfileApplication)
             return false;
         if (ProjectUtilities.isElementInAttachedProject(e))
             return false;
