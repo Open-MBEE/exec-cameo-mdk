7cbf595121e84d1ee0215fccd5967698bee9f1fc
Eric Brower <ewbrower@jpl.nasa.gov>
2015-06-02 11:20:32 -0700

    Updated signatures of Several new custom actions; altered SRConfigurator to be disabled when no relevant actions are available

--- src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/CopyAction.java
+++ src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/CopyAction.java
@@ -1,5 +1,16 @@
 package gov.nasa.jpl.mbee.actions.systemsreasoner;
 
-public class CopyAction {
+import com.nomagic.magicdraw.actions.MDAction;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
+
+public class CopyAction extends MDAction {
+	
+	public static final String actionid = "Despecialize";
+	public Element element; 
+	
+	public CopyAction(Element element) {
+        super(actionid, actionid, null, null);
+        this.element = element;
+	}
 
 }
--- src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/DespecializeAction.java
+++ src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/DespecializeAction.java
@@ -1,5 +1,16 @@
 package gov.nasa.jpl.mbee.actions.systemsreasoner;
 
-public class DespecializeAction {
+import com.nomagic.magicdraw.actions.MDAction;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 
+public class DespecializeAction extends MDAction {
+	
+	public static final String actionid = "Despecialize";
+	public Element element; 
+	
+	public DespecializeAction(Element element) {
+        super(actionid, actionid, null, null);
+        this.element = element;
+	}
+	
 }
