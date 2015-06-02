ed27c82bfe676b379b8a57dbfeeb919f5dcc1f65
Ivan Gomes <ivan.gomes@jpl.nasa.gov>
2015-06-02 10:20:28 -0700

    specialize action wip

--- src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/SpecializeAction.java
+++ src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/SpecializeAction.java
@@ -1,13 +1,20 @@
 package gov.nasa.jpl.mbee.actions.systemsreasoner;
 
+import gov.nasa.jpl.mbee.ems.ExportUtility;
+import gov.nasa.jpl.mbee.ems.ValidateModelRunner;
+
+import java.awt.event.ActionEvent;
+
+import com.nomagic.magicdraw.actions.MDAction;
 import com.nomagic.magicdraw.core.Application;
+import com.nomagic.ui.ProgressStatusRunner;
 import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
 import com.nomagic.magicdraw.openapi.uml.SessionManager;
 import com.nomagic.magicdraw.openapi.uml.ModelElementsManager;
 import com.nomagic.uml2.ext.jmi.helpers.ModelHelper;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class;
 import com.nomagic.uml2.ext.magicdraw.classes.mdinterfaces.Interface;
-
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Property;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.AggregationKindEnum;
@@ -21,11 +28,32 @@ import com.nomagic.uml2.ext.magicdraw.activities.mdfundamentalactivities.Activit
 import com.nomagic.uml2.ext.magicdraw.statemachines.mdbehaviorstatemachines.StateMachine;
 import com.nomagic.uml2.ext.magicdraw.interactions.mdbasicinteractions.Interaction;
 import com.nomagic.uml2.ext.magicdraw.commonbehaviors.mdbasicbehaviors.OpaqueBehavior;
-
 import com.nomagic.uml2.ext.magicdraw.commonbehaviors.mdsimpletime.DurationConstraint;
 import com.nomagic.uml2.ext.magicdraw.commonbehaviors.mdsimpletime.TimeConstraint;
 import com.nomagic.uml2.ext.magicdraw.interactions.mdfragments.InteractionConstraint;
 
-public class SpecializeAction {
+public class SpecializeAction extends MDAction {
+	
+	public static final String actionid = "Specialize";
+	public Element element;
+	
+	public SpecializeAction(Element element) {
+        super(actionid, actionid, null, null);
+        this.element = element;
+    }
 
+	@Override
+    public void actionPerformed(ActionEvent e) {
+		if (!(element instanceof Class)) {
+			return;
+		}
+		
+		final Class clazz = (Class) element;
+		//clazz.
+		
+        /*if (!ExportUtility.checkBaseline()) {    
+            return;
+        }
+        ProgressStatusRunner.runWithProgressStatus(new ValidateModelRunner(start), "Validating Model", true, 0);*/
+    }
 }
