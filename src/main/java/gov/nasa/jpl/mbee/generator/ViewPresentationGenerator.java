6dfc7a03989e89eeb1f0be0074623af2d7c22235
Eric Brower <ewbrower@jpl.nasa.gov>
2015-06-16 16:19:12 -0700

    Built function that returns the doc path hierarchy as a stack

--- src/main/java/gov/nasa/jpl/mbee/generator/ViewPresentationGenerator.java
+++ src/main/java/gov/nasa/jpl/mbee/generator/ViewPresentationGenerator.java
@@ -3,6 +3,7 @@ package gov.nasa.jpl.mbee.generator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import org.json.simple.JSONArray;
 
@@ -21,8 +22,10 @@ import com.nomagic.magicdraw.openapi.uml.SessionManager;
 import com.nomagic.uml2.ext.jmi.helpers.ModelHelper;
 import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
 import com.nomagic.uml2.ext.magicdraw.classes.mddependencies.Dependency;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Association;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Classifier;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Constraint;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.DirectedRelationship;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.ElementValue;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Expression;
@@ -32,7 +35,12 @@ import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.LiteralString;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.NamedElement;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Property;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Relationship;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Slot;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Type;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.TypedElement;
+import com.nomagic.uml2.ext.magicdraw.compositestructures.mdinternalstructures.Connector;
+import com.nomagic.uml2.ext.magicdraw.compositestructures.mdinternalstructures.ConnectorEnd;
 import com.nomagic.uml2.ext.magicdraw.mdprofiles.Stereotype;
 import com.nomagic.uml2.impl.ElementsFactory;
 
@@ -84,9 +92,7 @@ public class ViewPresentationGenerator {
             
         SessionManager.getInstance().createSession("view instance gen");
         try {
-            for (Element v: view2pe.keySet()) {
-                handleViewOrSection(v, null, view2pe.get(v));
-            }
+        	viewInstanceBuilder(view2pe);
             for (Element v: view2elements.keySet()) {
                 JSONArray es = view2elements.get(v);
                 StereotypesHelper.setStereotypePropertyValue(v, Utils.getViewClassStereotype(), "elements", es.toJSONString());
@@ -106,8 +112,49 @@ public class ViewPresentationGenerator {
         Utils.displayValidationWindow(vss, "Images Validation");
     }
     
+    private Stack<String> findRoot(Element elem, Stack<String> path) {
+    	// add the proper name of this element to the path, so we can use it later
+    	// when we are building our package structure
+    	path.push(((NamedElement)elem).getName() + " Presentation Elements");
+    	for (Relationship r: elem.get_relationshipOfRelatedElement()) {
+    		if (r instanceof Association) {
+    			// Associations have owned ends, which tell you which element owns which other element
+    			Association asso = (Association) r;
+    			for (Property prop: asso.getOwnedEnd()) {
+    				// for every incoming association (arrow pointing at this element)
+    				// there are properties with the name of the source of the association
+    				// for every outgoing association (arrow pointing away from this element)
+    				// there are properties with the name of this element
+    				if (!prop.getType().getName().equals(((NamedElement)elem).getName())) {
+    					// this is usually always true
+    					if (prop.getType() instanceof Element) {
+    						// go find the root of the root you just found
+    						path = findRoot(prop.getType(), path);
+    					}
+    				}
+    			}
+    		}
+    	}
+		return path;
+    }
+    
+    private void viewInstanceBuilder(Map<Element, List<PresentationElement>> view2pe) {
+    	// find the doc path
+    	for (Element v: view2pe.keySet()) {
+    		Stack<String> docPath = findRoot(v, new Stack<String>());
+    		System.out.println("path:\t" + docPath);
+    	}
+        for (Element v: view2pe.keySet()) {
+            handleViewOrSection(v, null, view2pe.get(v));
+        }
+    }
+    
     private void handleViewOrSection(Element view, InstanceSpecification section, List<PresentationElement> pes) {
-        Package owner = getFolder(view);
+    	Package owner = getFolder(view);
+    	handleViewOrSection(view, section, pes, owner);
+    }
+    
+    private void handleViewOrSection(Element view, InstanceSpecification section, List<PresentationElement> pes, Package owner) {
         List<InstanceValue> list = new ArrayList<InstanceValue>();
         for (PresentationElement pe: pes) {
             if (pe.isManual()) {
@@ -186,8 +233,9 @@ public class ViewPresentationGenerator {
     
     private Package getFolder(Element view) {
         List<Element> results = Utils.collectDirectedRelatedElementsByRelationshipStereotype(view, presentsS, 1, false, 1);
-        if (!results.isEmpty() && results.get(0) instanceof Package)
-            return (Package)results.get(0);
+        if (!results.isEmpty() && results.get(0) instanceof Package) {
+        	return (Package)results.get(0);
+        }
         Element result = view.getOwner();
         while(!(result instanceof Package)) {
             result = result.getOwner();
