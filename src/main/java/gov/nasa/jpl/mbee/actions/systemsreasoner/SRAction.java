ae8bd2ef7fde61f044b0a89b98be310c8d20da35
Ivan Gomes <ivan.gomes@jpl.nasa.gov>
2015-06-22 12:36:43 -0700

    fixing SRAction

--- src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/SRAction.java
+++ src/main/java/gov/nasa/jpl/mbee/actions/systemsreasoner/SRAction.java
@@ -6,45 +6,38 @@ import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 
 public class SRAction extends MDAction {
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	// actuallyUseful is just a class var that keeps track of whether the action is
-	// enabled or disable in an 'actually useful' way
->>>>>>> 9733057... Systems Reasoner now supports multiple selected elements
-	private boolean actuallyUseful = true;
-=======
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
->>>>>>> 55457c7... Housekeeping
 	private String actionid;
 	public Element element;
 
 	public SRAction(String actionid) {
         super(actionid, actionid, null, ActionsGroups.APPLICATION_RELATED);
-<<<<<<< HEAD
-=======
 		this.actionid = actionid;
 	}
 	
 	public SRAction(String actionid, Element element) {
 		this(actionid);
         this.element = element;
->>>>>>> e5c673c... Altered where element attached to Actions is stored, now in the superclass
     }
 	
 	public void disable() {
-		this.setEnabled(actuallyUseful = false);
+		setEnabled(false);
+	}
+	
+	public void disable(String error) {
+		this.setName(actionid + " [" + error + "]");
+		disable();
 	}
 	
 	public void enable() {
-		this.setEnabled(actuallyUseful = true);
+		setEnabled(true);
 	}
 	
 	@Override
 	public void updateState() {
-		this.setEnabled(actuallyUseful);
 	}
+	
 }
