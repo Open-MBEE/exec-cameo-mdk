d91500d8847a428ae518f653c80dfba79272bf64
Aaron Black <aaron.black@jpl.nasa.gov>
2016-10-19 10:03:30 -0700

    initial test dump

---
+++ src/test/java/gov/nasa/jpl/mbee/mdk/test/TeamworkCloud.java
@@ -0,0 +1,72 @@
+package gov.nasa.jpl.mbee.mdk.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+public class TeamworkCloud extends MagicDrawTests {
+    public static void main(String[] args) throws Exception
+    {
+        twc_SetDefaults();
+        parseArgs(args);
+        //System.out.println(" --------------- In main method");
+        JUnitCore jCore = new JUnitCore();
+        Result results = jCore.run(TeamworkCloud.class);
+        printJunitResults(results);
+        System.exit(0);
+    }
+
+    public TeamworkCloud (String testMethodToRun, String testName)
+    {
+        super(testMethodToRun, testName);
+        //setDoNotUseSilentMode(false);
+        System.setProperty("jsse.enableSNIExtension", "false");
+        setSkipMemoryTest(true);
+        //setDoNotUseSilentMode(true);
+    }
+
+    public static Test suite() throws Exception
+    {
+
+
+        TestSuite suite = new TestSuite();
+
+
+//        suite.addTest(new TeamworkCloud("openProject", "Open Project"));
+  //      suite.addTest(new TeamworkCloud("add100Blocks", "Add 100 Blocks"));
+//        suite.addTest(new TeamworkCloud("deleteCreatedElements", "Delete all elements created"));
+
+//        suite.addTest(new TeamworkCloud("openProject", "Open Project"));
+//        suite.addTest(new TeamworkCloud("add100Blocks", "Add 100 Blocks"));
+ //       suite.addTest(new TeamworkCloud("deleteCreatedElements", "Delete all elements created"));
+
+
+//        suite.addTest(new TeamworkCloud("add1000Blocks", "Add 1000 Blocks"));
+//        suite.addTest(new TeamworkCloud("add100Diagrams", "Add 100 Diagrams"));
+//        suite.addTest(new TeamworkCloud("deleteCreatedElements", "Delete all elements created"));
+//
+//        suite.addTest(new TeamworkCloud("saveProjectOutput", "SaveProject"));
+//        suite.addTest(new TeamworkCloud("closeProject", "Close Project"));
+
+        //suite.addTest(new TeamworkCloud("closeProject", "Close Project"));
+
+        //suite.addTest(new TeamworkCloud("twcLogin", "Logging in to TWC"));
+
+        suite.addTest( new TeamworkCloud("twcs_Login", "Login to Teamwork Cloud"));
+        suite.addTest( new TeamworkCloud("twcs_AddProjectTest", "Add New project to Teamwork Cloud"));
+        suite.addTest( new TeamworkCloud("twcs_OpenProjectTest", "Open Project from Teamwork Cloud"));
+        suite.addTest( new TeamworkCloud("twcs_AddDeleteElementsTest1", "Add/Commit/Delete/Commit 100 blocks"));
+        suite.addTest( new TeamworkCloud("twcs_AddDeleteElementsTest2", "Add/Commit/Delete/Commit 1000 blocks"));
+        suite.addTest( new TeamworkCloud("twcs_AddDeleteElementsTest3", "Add/Commit/Delete/Commit 100 diagrams"));
+        suite.addTest( new TeamworkCloud("twcs_UpdateProjectTest", "Update Project from Teamwork Cloud"));
+
+        suite.addTest( new TeamworkCloud("twcs_CreateBranchTest", "Create a Project Branch"));
+        suite.addTest( new TeamworkCloud("twcs_OpenBranchTest", "Open a Project Branch"));
+        suite.addTest( new TeamworkCloud("twcs_LockModelTest", "Lock Entire Model Recursively"));
+        suite.addTest( new TeamworkCloud("twcs_UnlockModelTest", "Unlock Entire Model Recursively"));
+        suite.addTest( new TeamworkCloud("twcs_Logout", "Log out of Teamwork Cloud"));
+
+        return suite;
+    }
+}
---
+++ src/test/java/gov/nasa/jpl/mbee/mdk/test/integration/CoordinatedSyncMDElementCreate.java
@@ -0,0 +1,120 @@
+package gov.nasa.jpl.mbee.mdk.test.integration;
+
+import gov.nasa.jpl.mbee.mdk.test.MDKTestCase;
+import org.json.simple.JSONObject;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+/**
+ * @author ablack
+ * @JIRA MAGICDRAW-263
+ *
+ */
+public class CoordinatedSyncMDElementCreate extends MDKTestCase
+{
+    //global test vars
+    static String testName = "CoordinatedSyncMDElementCreate";
+    static String targetSysMLID;
+    static Element targetPackage;
+    static Element targetElement;
+    
+    public static void main(String[] args) throws Exception 
+    {   
+        parseArgs(args);
+        System.out.println("In main method");
+        JUnitCore jCore = new JUnitCore();
+        Result results = jCore.run(CoordinatedSyncMDElementCreate.class);
+        printJunitResults(results);
+        System.exit(0);
+    }
+
+    public CoordinatedSyncMDElementCreate (String testMethodToRun, String testName) 
+    {
+        super(testMethodToRun, testName);
+        setDoNotUseSilentMode(true);
+        System.setProperty("jsse.enableSNIExtension", "false");
+        setSkipMemoryTest(true);
+        setDoNotUseSilentMode(true);    
+    }
+/*
+    public static Test suite() throws Exception
+    {
+        TestSuite suite = new TestSuite();        
+        
+        suite.addTest(new CoordinatedSyncMDElementCreate("executeTest", "execute test"));
+        
+        return suite;
+    }
+    
+    @Override
+    protected void setUpTest() throws Exception {
+        super.setUpTest();
+        // do setup here
+        
+        // load credentials from file
+        loadCredentials("");
+
+        // set MMS credentials
+        MDKHelper.setMMSLoginCredentials(teamworkUsername, teamworkPassword);
+        
+        //open project
+        projectName = "CoordinatedSyncTest";
+        testProject = testRoot + "/mdk/resource/" + projectName + ".mdzip";
+        super.openProject();
+        
+        //clean and prepare test environment
+        MagicDrawHelper.createSession();
+        MagicDrawHelper.deleteEditableContainerChildren(ElementFinder.getModelRoot());
+        MagicDrawHelper.closeSession();
+        
+        //make sure expected stuff is in place
+        MagicDrawHelper.createSession();
+        targetPackage = MagicDrawHelper.createPackage("CreatePkg", ElementFinder.getModelRoot());
+        MagicDrawHelper.closeSession();
+        super.saveUpdatedProject();
+    }
+
+    public void executeTest() {
+        //testing vars
+        String updatedDocumentation = "New documentation";
+        String specType = "Product";
+
+        //confirm mms permissions
+        super.confirmMMSPermissions();
+        
+        //create / change / delete
+        MagicDrawHelper.createSession();
+        targetElement = MagicDrawHelper.createDocument("CreateDoc", targetPackage);
+        MagicDrawHelper.setElementDocumentation(targetElement, updatedDocumentation);
+        MagicDrawHelper.closeSession();
+                
+        // save model to push changes
+        super.saveUpdatedProject();
+        
+        // confirm via rest
+        JSONObject jo = MDKHelper.getMmsElement(targetElement);
+        if (!jo.get("sysmlid").equals(targetElement.getID())) {
+            fail("Returned element sysmlid does not match returned json");
+        }
+        JSONObject joSpec = (JSONObject) jo.get("specialization");
+        if (!joSpec.get("type").equals(specType)) {
+            fail("Returned element type does not match " + specType);
+        }
+    }
+    
+    @Override
+    protected void tearDownTest() throws Exception {
+        super.tearDownTest();
+        // do tear down here
+        
+        //close project
+        super.closeProject();
+    }
+*/
+}
