3222774fac6e14fa0ab65980a81ad5e68f2c9327
Aaron Black <aaron.black@jpl.nasa.gov>
2016-12-05 08:28:53 -0800

    MAGICDRAW-493 #comment Options removed from user control, pull request still pending

--- src/main/java/gov/nasa/jpl/mbee/options/MDKOptionsGroup.java
+++ src/main/java/gov/nasa/jpl/mbee/options/MDKOptionsGroup.java
@@ -49,12 +49,7 @@ public class MDKOptionsGroup extends AbstractPropertyOptionsGroup {
         BooleanProperty property = new BooleanProperty(PERSIST_CHANGELOG, value);
         property.setResourceProvider(PROPERTY_RESOURCE_PROVIDER);
         property.setGroup(GROUP);
-        if (MDUtils.isDeveloperMode()) {
-            addProperty(property);
-        } else {
-            addInvisibleProperty(property);
-        }
-
+        addInvisibleProperty(property);
     }
     
     public boolean isChangeListenerEnabled() {
@@ -66,12 +61,7 @@ public class MDKOptionsGroup extends AbstractPropertyOptionsGroup {
         BooleanProperty property = new BooleanProperty(CHANGE_LISTENER, value);
         property.setResourceProvider(PROPERTY_RESOURCE_PROVIDER);
         property.setGroup(GROUP);
-        if (MDUtils.isDeveloperMode()) {
-            addProperty(property);
-        } else {
-            addInvisibleProperty(property);
-        }
-
+        addInvisibleProperty(property);
     }
 
     public boolean isCoordinatedSyncEnabled() {
@@ -83,12 +73,7 @@ public class MDKOptionsGroup extends AbstractPropertyOptionsGroup {
         BooleanProperty property = new BooleanProperty(COORDINATED_SYNC, value);
         property.setResourceProvider(PROPERTY_RESOURCE_PROVIDER);
         property.setGroup(GROUP);
-        if (MDUtils.isDeveloperMode()) {
-            addProperty(property);
-        } else {
-            addInvisibleProperty(property);
-        }
-
+        addInvisibleProperty(property);
     }
     
     public static final PropertyResourceProvider PROPERTY_RESOURCE_PROVIDER = new PropertyResourceProvider() {
