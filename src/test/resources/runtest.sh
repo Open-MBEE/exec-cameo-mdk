d80babdea69024499351fe1b19499fceb3fa978a
Aaron Black <aaron.black@jpl.nasa.gov>
2016-12-16 10:10:27 -0800

    Blocking unneeded tests. Modified runtest to autoexpand needed jars

--- src/test/resources/runtest.sh
+++ src/test/resources/runtest.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-#echo Credentials Location: $1
+#echo Test JARs Directory : $1
+#echo MD Install Directory : $2
+#echo Test Class Name : $3
 
 #run command from MD install dir
-cd /Users/ablack/git/mdk/build/install
+cd $2
+
+export MDKTESTJARS=$(find $1 -name "*.jar" | tr -s '\r\n' ':')
+export MDKTESTJARS=${MDKTESTJARS/%?/}
+export MDKJARS=$(find $2/plugins/gov.nasa.jpl.mbee.mdk -name "*.jar" | tr -s '\r\n' ':')
+export MDKJARS=${MDKJARS/%?/}
 
 #all additional needed JARs must be added individually to the -Dmd.additional.class.path property like the ones below
 java -Xmx4096M -Xss1024M -DLOCALCONFIG=true -DWINCONFIG=true \
@@ -14,19 +21,7 @@ java -Xmx4096M -Xss1024M -DLOCALCONFIG=true -DWINCONFIG=true \
 `         `lib/bundles/com.nomagic.magicdraw.osgi.fragment_1.0.0.201607250742.jar:\
 `         `lib/md_api.jar:lib/md_common_api.jar:lib/md.jar:lib/md_common.jar:lib/jna.jar \
        -Dmd.class.path=file:bin/magicdraw.properties#CLASSPATH \
-       -Dmd.additional.class.path=../libs/mdk-3.0-SNAPSHOT.jar:\
-`         `../libs/mdk-3.0-SNAPSHOT-tests.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/jackson-databind-2.8.3.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/jackson-annotations-2.8.0.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/jackson-core-2.8.3.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/jackson-coreutils-1.6.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/commons-codec-1.9.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/commons-io-1.3.2.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/commons-logging-1.2.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/httpclient-4.5.2.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/httpcore-4.4.4.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/httpmime-4.5.2.jar:\
-`         `plugins/gov.nasa.jpl.mbee.mdk/activemq-all-5.9.1.jar \
+       -Dmd.additional.class.path=${MDKTESTJARS}:${MDKJARS} \
        -Dcom.sun.media.imageio.disableCodecLib=true \
        -noverify \
        -Dcom.nomagic.osgi.config.dir=configuration \
@@ -34,4 +29,4 @@ java -Xmx4096M -Xss1024M -DLOCALCONFIG=true -DWINCONFIG=true \
        -Dlogback.configurationFile=data/logback.xml \
        -Dsun.locale.formatasdefault=true \
        -Dcom.nomagic.magicdraw.launcher=com.nomagic.osgi.launcher.junit_support.JUnitCoreWrapper \
-       com.nomagic.osgi.launcher.ProductionFrameworkLauncher gov.nasa.jpl.mbee.mdk.test.tests.$1
+       com.nomagic.osgi.launcher.ProductionFrameworkLauncher gov.nasa.jpl.mbee.mdk.test.tests.$3
