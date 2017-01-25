a011078ae11c70f43811561afc3520e88a9d2ac6
Johannes Gross <johannes.gross@jpl.nasa.gov>
2017-01-25 15:35:27 -0800

    Fixed comments.

--- src/main/java/gov/nasa/jpl/mbee/mdk/generator/ProductViewParser.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/generator/ProductViewParser.java
@@ -1,31 +1,3 @@
-/*******************************************************************************
- * Copyright (c) <2013>, California Institute of Technology ("Caltech").
- * U.S. Government sponsorship acknowledged.
- * <p>
- * All rights reserved.
- * <p>
- * Redistribution and use in source and binary forms, with or without modification, are
- * permitted provided that the following conditions are met:
- * <p>
- * - Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * - Neither the name of Caltech nor its operating division, the Jet Propulsion Laboratory,
- * nor the names of its contributors may be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * <p>
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************/
 package gov.nasa.jpl.mbee.mdk.generator;
 
 import com.nomagic.magicdraw.core.Application;
@@ -86,7 +58,8 @@ public class ProductViewParser {
                     productS, "excludeViews");
             this.noSections = new HashSet<Element>(noSections);
             this.excludeViews = new HashSet<Element>(excludeViews);
-        } else {
+        }
+        else {
             noSections = new HashSet<Element>();
             excludeViews = new HashSet<Element>();
         }
@@ -101,7 +74,8 @@ public class ProductViewParser {
             Section chapter1 = dg.parseView(start);
             top = chapter1;
             doc.addElement(chapter1);
-        } else {
+        }
+        else {
             Section s = dg.parseView(start);
             for (DocGenElement e : s.getChildren()) {
                 top.addElement(e);
@@ -119,8 +93,9 @@ public class ProductViewParser {
      */
     private void parseView(Class view, Container parent, boolean nosection, boolean recurse) {
         if (visitedViews.contains(view)) {
-            Application.getInstance().getGUILog().log("[WARNING] View " + view.getName() + " is visited in circular reference! Skipping view.");
-        } else {
+            Application.getInstance().getGUILog().log("[WARNING] View " + view.getName() + " has already been visited! Skipping view.");
+        }
+        else {
             visitedViews.add(view);
             String viewname = view.getName();
             Section viewSection = dg.parseView(view);
@@ -147,7 +122,8 @@ public class ProductViewParser {
             }
             if (noSections.contains(prop) || noSections.contains(type)) {
                 childNoSections.add(new Pair<>(type, prop.getAggregation()));
-            } else {
+            }
+            else {
                 childSections.add(new Pair<>(type, prop.getAggregation()));
             }
         }
