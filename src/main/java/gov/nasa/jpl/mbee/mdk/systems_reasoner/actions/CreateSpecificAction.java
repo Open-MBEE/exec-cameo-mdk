cff02276702bd4f3058ccccbe72ea02c2452a08f
Johannes Gross <johannes.gross@jpl.nasa.gov>
2017-02-13 16:53:22 -0800

    MAGICDRAW-202
    Changed logic to not delete constraints from owned member list of classes.

--- src/main/java/gov/nasa/jpl/mbee/mdk/systems_reasoner/actions/CreateSpecificAction.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/systems_reasoner/actions/CreateSpecificAction.java
@@ -83,11 +83,11 @@ public class CreateSpecificAction extends SRAction {
             int i = 0;
             for (BaseElement specific : specifics) {
                 if (specific instanceof Classifier) {
-                    //System.out.println(((Classifier) specific).getName());
+                    System.out.println(((Classifier) specific).getName());
                     Collection<NamedElement> redefElements = new ArrayList<>();
                     ((Classifier) specific).getGeneralization().retainAll(redefElements);
                     for (NamedElement ne : ((Namespace) specific).getOwnedMember()) {
-                        if (ne instanceof RedefinableElement) {
+                        if (ne instanceof RedefinableElement || ne instanceof Constraint) {
                             // Dont throw away those we want to redefine.
                             redefElements.add(ne);
                         }
