cd2f915c59638448c43d7fe7a70200a344e943ae
Gross, Johannes (355A) <Johannes.Gross@jpl.nasa.gov>
2017-04-19 11:17:31 -0700

    MAGICDRAW-417
    Fixed generic table creation, skipping two ominously appearing columns.

--- src/main/java/gov/nasa/jpl/mbee/mdk/model/GenericTable.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/model/GenericTable.java
@@ -3,14 +3,17 @@ package gov.nasa.jpl.mbee.mdk.model;
 import com.nomagic.generictable.GenericTableManager;
 import com.nomagic.magicdraw.core.Application;
 import com.nomagic.magicdraw.openapi.uml.SessionManager;
+import com.nomagic.magicdraw.properties.ElementListProperty;
+import com.nomagic.magicdraw.properties.ElementProperty;
+import com.nomagic.magicdraw.properties.Property;
+import com.nomagic.magicdraw.properties.StringProperty;
 import com.nomagic.uml2.ext.jmi.helpers.ModelHelper;
 import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Diagram;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.NamedElement;
 import gov.nasa.jpl.mbee.mdk.docgen.DocGenProfile;
-import gov.nasa.jpl.mbee.mdk.docgen.docbook.DBTable;
-import gov.nasa.jpl.mbee.mdk.docgen.docbook.DBText;
-import gov.nasa.jpl.mbee.mdk.docgen.docbook.DocumentElement;
+import gov.nasa.jpl.mbee.mdk.docgen.docbook.*;
 import gov.nasa.jpl.mbee.mdk.generator.DiagramTableTool;
 import gov.nasa.jpl.mbee.mdk.util.GeneratorUtils;
 import gov.nasa.jpl.mbee.mdk.util.Utils;
@@ -23,9 +26,44 @@ public class GenericTable extends Table {
 
     private List<String> headers;
     private boolean skipIfNoDoc;
+    private static ArrayList<String> skipColumnIDs = new ArrayList<String>() {{
+        add("QPROP:Element:isEncapsulated");
+        add("QPROP:Element:CUSTOM_IMAGE");
+    }};
+    private int numCols = 0;
 
-    @SuppressWarnings("unchecked")
-    public List<List<DocumentElement>> getHeaders(Diagram d, List<String> columnIds, DiagramTableTool dtt) {
+//    @SuppressWarnings("unchecked")
+//    public List<List<DocumentElement>> getHeaders(Diagram d, List<String> columnIds, DiagramTableTool dtt) {
+//        List<List<DocumentElement>> res = new ArrayList<List<DocumentElement>>();
+//        if (this.headers != null && !this.headers.isEmpty()) {
+//            List<DocumentElement> row = new ArrayList<DocumentElement>();
+//            for (String h : this.headers) {
+//                row.add(new DBText(h));
+//            }
+//            res.add(row);
+//        } else if (StereotypesHelper.hasStereotypeOrDerived(d, DocGenProfile.headersChoosable)) {
+//            List<DocumentElement> row = new ArrayList<DocumentElement>();
+//            for (String h : (List<String>) StereotypesHelper.getStereotypePropertyValue(d, DocGenProfile.headersChoosable, "headers")) {
+//                row.add(new DBText(h));
+//            }
+//            res.add(row);
+//        } else {
+//            List<DocumentElement> row = new ArrayList<DocumentElement>();
+//            int count = 0;
+//            for (String s : dtt.getColumnNames(d, columnIds)) {
+//                if (count == 0) {
+//                    count++;
+//                    continue;
+//                }
+//
+//                row.add(new DBText(s));
+//            }
+//            res.add(row);
+//        }
+//        return res;
+//    }
+
+    public List<List<DocumentElement>> getHeaders(Diagram d, List<String> columnIds, GenericTableManager gtm) {
         List<List<DocumentElement>> res = new ArrayList<List<DocumentElement>>();
         if (this.headers != null && !this.headers.isEmpty()) {
             List<DocumentElement> row = new ArrayList<DocumentElement>();
@@ -42,19 +80,21 @@ public class GenericTable extends Table {
         } else {
             List<DocumentElement> row = new ArrayList<DocumentElement>();
             int count = 0;
-            for (String s : dtt.getColumnNames(d, columnIds)) {
+            for (String columnid : columnIds) {
                 if (count == 0) {
                     count++;
                     continue;
                 }
-                row.add(new DBText(s));
+                if (!skipColumnIDs.contains(columnid)) {
+                    row.add(new DBText(gtm.getColumnNameById(d, columnid)));
+                    numCols++;
+                }
             }
             res.add(row);
         }
         return res;
 
     }
-
 //    public List<List<DocumentElement>> getBody(Diagram d, List<Element> rowElements, List<String> columnIds,
 //                                               DiagramTableTool dtt, boolean forViewEditor) {
 //        List<List<DocumentElement>> res = new ArrayList<>();
@@ -90,7 +130,33 @@ public class GenericTable extends Table {
                     count++;
                     continue;
                 }
-                row.add(Common.getTableEntryFromObject(getTableValues(gtm.getCellValue(d, e, cid))));
+                if (skipColumnIDs.contains(cid)) {
+                    continue;
+                }
+                //   row.add(Common.getTableEntryFromObject(getTableValues(gtm.getCellValue(d, e, cid))));
+                //row.add(getTableValues(gtm.getCellValue(d, e, cid)));
+
+                DBTableEntry entry = new DBTableEntry();
+                Property cellValue = gtm.getCellValue(d, e, cid);
+                if (cellValue instanceof ElementProperty) {
+                    Element cellelement = ((ElementProperty) cellValue).getElement();
+                    if (cellelement instanceof NamedElement) {
+                        entry.addElement(new DBParagraph(((NamedElement) cellelement).getName(), cellelement, From.NAME));
+                    }
+                } else if (cellValue instanceof StringProperty) {
+                    entry.addElement(new DBParagraph(cellValue.getValue()));
+                } else if (cellValue instanceof ElementListProperty) {
+                    ElementListProperty elp = (ElementListProperty) cellValue;
+                    for (Element listEl : ((ElementListProperty) cellValue).getValue()) {
+                        if (listEl instanceof NamedElement) {
+                            entry.addElement(new DBParagraph(((NamedElement) listEl).getName(), listEl, From.NAME));
+                        }
+                    }
+                } else {
+                    System.out.print("Not added : " + cellValue.toString() + "    ");
+                }
+                row.add(entry);
+
             }
             res.add(row);
         }
@@ -131,7 +197,7 @@ public class GenericTable extends Table {
             return res;
         }
 
-        SessionManager.getInstance().createSession(Application.getInstance().getProject(), "Hello");
+        SessionManager.getInstance().createSession(Application.getInstance().getProject(), "Reading Generic Table");
         int tableCount = 0;
         List<Object> targets = isSortElementsByName() ? Utils.sortByName(getTargets()) : getTargets();
         for (Object e : targets) {
@@ -172,7 +238,8 @@ public class GenericTable extends Table {
 //                    }
                     List<String> columnIds = gtm.getColumnIds(diagram);
                     //List<String> columnIds = dtt.getColumnIds(diagram);
-                    t.setHeaders(getHeaders(diagram, columnIds, dtt));
+                    t.setHeaders(getHeaders(diagram, columnIds, gtm));
+                    //t.setHeaders(getHeaders(diagram, columnIds, dtt));
                     List<Element> rowElements = gtm.getRowElements(diagram);
                     //List<Element> rowElements = dtt.getRowElements(diagram);
                     t.setBody(getBody(diagram, rowElements, columnIds, gtm, forViewEditor));
@@ -187,7 +254,9 @@ public class GenericTable extends Table {
                     } else {
                         t.setCaption(ModelHelper.getComment(diagram));
                     }
-                    t.setCols(columnIds.size() - 1);
+                    //t.setCols(columnIds.size() - 1);
+
+                    t.setCols(numCols);
                     res.add(t);
                     t.setStyle(getStyle());
                     tableCount++;
