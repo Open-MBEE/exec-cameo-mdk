b2fd9b5b1f998ba14afde83b80dccd94639c93b5
Gross, Johannes (355A) <Johannes.Gross@jpl.nasa.gov>
2017-04-19 11:36:45 -0700

    Cleanup

--- src/main/java/gov/nasa/jpl/mbee/mdk/model/GenericTable.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/model/GenericTable.java
@@ -14,7 +14,6 @@ import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.NamedElement;
 import gov.nasa.jpl.mbee.mdk.docgen.DocGenProfile;
 import gov.nasa.jpl.mbee.mdk.docgen.docbook.*;
-import gov.nasa.jpl.mbee.mdk.generator.DiagramTableTool;
 import gov.nasa.jpl.mbee.mdk.util.GeneratorUtils;
 import gov.nasa.jpl.mbee.mdk.util.Utils;
 
@@ -32,36 +31,6 @@ public class GenericTable extends Table {
     }};
     private int numCols = 0;
 
-//    @SuppressWarnings("unchecked")
-//    public List<List<DocumentElement>> getHeaders(Diagram d, List<String> columnIds, DiagramTableTool dtt) {
-//        List<List<DocumentElement>> res = new ArrayList<List<DocumentElement>>();
-//        if (this.headers != null && !this.headers.isEmpty()) {
-//            List<DocumentElement> row = new ArrayList<DocumentElement>();
-//            for (String h : this.headers) {
-//                row.add(new DBText(h));
-//            }
-//            res.add(row);
-//        } else if (StereotypesHelper.hasStereotypeOrDerived(d, DocGenProfile.headersChoosable)) {
-//            List<DocumentElement> row = new ArrayList<DocumentElement>();
-//            for (String h : (List<String>) StereotypesHelper.getStereotypePropertyValue(d, DocGenProfile.headersChoosable, "headers")) {
-//                row.add(new DBText(h));
-//            }
-//            res.add(row);
-//        } else {
-//            List<DocumentElement> row = new ArrayList<DocumentElement>();
-//            int count = 0;
-//            for (String s : dtt.getColumnNames(d, columnIds)) {
-//                if (count == 0) {
-//                    count++;
-//                    continue;
-//                }
-//
-//                row.add(new DBText(s));
-//            }
-//            res.add(row);
-//        }
-//        return res;
-//    }
 
     public List<List<DocumentElement>> getHeaders(Diagram d, List<String> columnIds, GenericTableManager gtm) {
         List<List<DocumentElement>> res = new ArrayList<List<DocumentElement>>();
@@ -96,26 +65,6 @@ public class GenericTable extends Table {
 
     }
 
-//    public List<List<DocumentElement>> getBody(Diagram d, List<Element> rowElements, List<String> columnIds,
-//                                               DiagramTableTool dtt, boolean forViewEditor) {
-//        List<List<DocumentElement>> res = new ArrayList<>();
-//        for (Element e : rowElements) {
-//            if (skipIfNoDoc && ModelHelper.getComment(e).trim().isEmpty()) {
-//                continue;
-//            }
-//            List<DocumentElement> row = new ArrayList<>();
-//            int count = 0;
-//            for (String cid : columnIds) {
-//                if (count == 0) {
-//                    count++;
-//                    continue;
-//                }
-//                row.add(Common.getTableEntryFromObject(getTableValues(dtt.getCellValue(d, e, cid))));
-//            }
-//            res.add(row);
-//        }
-//        return res;
-//    }
 
     public List<List<DocumentElement>> getBody(Diagram d, List<Element> rowElements, List<String> columnIds,
                                                GenericTableManager gtm, boolean forViewEditor) {
@@ -151,7 +100,7 @@ public class GenericTable extends Table {
                         }
                     }
                 } else {
-                    System.out.print("[WARNING] Not added : " + cellValue.toString() + "    ");
+                    System.out.print("[WARNING] Not added : " + cellValue.toString() + ".");
                 }
                 row.add(entry);
 
@@ -190,7 +139,6 @@ public class GenericTable extends Table {
     @Override
     public List<DocumentElement> visit(boolean forViewEditor, String outputDir) {
         List<DocumentElement> res = new ArrayList<DocumentElement>();
-        DiagramTableTool dtt = new DiagramTableTool();
         if (getIgnore()) {
             return res;
         }
@@ -206,42 +154,11 @@ public class GenericTable extends Table {
                     DBTable t = new DBTable();
 
                     GenericTableManager gtm = new GenericTableManager();
-                    //gtm.getCellValue(diagram, , "");
-                    // String columnid = gtm.getColumnIDByPropertyName("Owner");
-//                    List<Element> list = gtm.getRowElements(diagram);
-//                    List<String> columns = gtm.getColumnIds(diagram);
-//
-//                    for (Element element : list) {
-//                        System.out.println("Row: " + element.getHumanName());
-//                        for (String columnid : columns) {
-//                            System.out.print("Column: " + columnid + "    ");
-//                            Property value = gtm.getCellValue(diagram, element, columnid);
-//                            if (value != null) {
-//                                System.out.print("CellValue: " + value.getName() + "    ");
-//                                if (value instanceof ElementProperty) {
-//                                    Element cellelement = ((ElementProperty) value).getElement();
-//                                    System.out.print("ElementProperty Element: " + cellelement.getHumanName() + "    ");
-//                                } else if (value instanceof StringProperty) {
-//                                    System.out.print("StringProperty: " + ((StringProperty) value).getString() + "    ");
-//                                } else if (value instanceof ElementListProperty) {
-//                                    ElementListProperty elp = (ElementListProperty) value;
-//                                    for (Element listEl : ((ElementListProperty) value).getValue()) {
-//                                        System.out.println("listel: " + listEl.getHumanName() + ", ");
-//                                    }
-//                                } else {
-//                                    System.out.print("Or: " + value.toString() + "    ");
-//                                }
-//                            }
-//                        }
-//                    }
+
                     List<String> columnIds = gtm.getColumnIds(diagram);
-                    //List<String> columnIds = dtt.getColumnIds(diagram);
                     t.setHeaders(getHeaders(diagram, columnIds, gtm));
-                    //t.setHeaders(getHeaders(diagram, columnIds, dtt));
                     List<Element> rowElements = gtm.getRowElements(diagram);
-                    //List<Element> rowElements = dtt.getRowElements(diagram);
                     t.setBody(getBody(diagram, rowElements, columnIds, gtm, forViewEditor));
-                    //t.setBody(getBody(diagram, rowElements, columnIds, dtt, forViewEditor));
                     if (getTitles() != null && getTitles().size() > tableCount) {
                         t.setTitle(getTitlePrefix() + getTitles().get(tableCount) + getTitleSuffix());
                     } else {
@@ -252,8 +169,6 @@ public class GenericTable extends Table {
                     } else {
                         t.setCaption(ModelHelper.getComment(diagram));
                     }
-                    //t.setCols(columnIds.size() - 1);
-
                     t.setCols(numCols);
                     res.add(t);
                     t.setStyle(getStyle());
@@ -263,7 +178,6 @@ public class GenericTable extends Table {
         }
 
         SessionManager.getInstance().closeSession(Application.getInstance().getProject());
-        dtt.closeOpenedTables();
         return res;
     }
 
