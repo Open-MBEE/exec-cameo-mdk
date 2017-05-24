2521e6631da05e7e5ad8d0e54bec8ba4c2a83635
Aaron Black <aaron.black@jpl.nasa.gov>
2017-05-24 12:07:28 -0700

    Fixed magicdraw message processing

--- src/main/java/gov/nasa/jpl/mbee/mdk/mms/sync/jms/JMSMessageListener.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/mms/sync/jms/JMSMessageListener.java
@@ -129,8 +129,7 @@ public class JMSMessageListener implements MessageListener, ExceptionListener {
         }
         else if (syncedJsonNode != null && syncedJsonNode.isObject()) {
             JsonNode senderJsonNode, sourceJsonNode;
-            if ((senderJsonNode = syncedJsonNode.get("sender")) != null && senderJsonNode.isTextual() && senderJsonNode.asText().equals(TicketUtils.getUsername(project))
-                    && (sourceJsonNode = messageJsonNode.get("source")) != null && sourceJsonNode.isTextual() && sourceJsonNode.asText().startsWith("magicdraw")) {
+            if ((sourceJsonNode = messageJsonNode.get("source")) != null && sourceJsonNode.isTextual() && sourceJsonNode.asText().startsWith("magicdraw")) {
                 return;
             }
             Changelog<String, Void> syncedChangelog = SyncElements.buildChangelog((ObjectNode) syncedJsonNode);
