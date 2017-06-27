4ba7d325166df4d3423258ebd8797ae388d3c08d
Ivan Gomes <ivan.gomes@jpl.nasa.gov>
2017-06-27 11:22:34 -0700

    Code clean-up

--- src/main/java/gov/nasa/jpl/mbee/mdk/util/Utils.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/util/Utils.java
@@ -5,9 +5,7 @@ import com.nomagic.magicdraw.annotation.Annotation;
 import com.nomagic.magicdraw.core.Application;
 import com.nomagic.magicdraw.core.GUILog;
 import com.nomagic.magicdraw.core.Project;
-import com.nomagic.magicdraw.core.project.ProjectDescriptor;
 import com.nomagic.magicdraw.core.project.ProjectDescriptorsFactory;
-import com.nomagic.magicdraw.core.project.ProjectsManager;
 import com.nomagic.magicdraw.esi.EsiUtils;
 import com.nomagic.magicdraw.ui.MainFrame;
 import com.nomagic.magicdraw.ui.dialogs.MDDialogParentProvider;
@@ -50,7 +48,6 @@ import org.apache.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.reflect.Field;
@@ -1758,19 +1755,22 @@ public class Utils {
         }
 
         Package dummyvs = (Package) project.getElementByID("_17_0_2_407019f_1354124289134_280378_12909");
-//        if (dummyvs == null) {
-//            try {
-//                ProjectsManager projectsManager = Application.getInstance().getProjectsManager();
-//                File file = new File(Application.environment().getProfilesDirectory() + "SysML Extensions.mdxml");
-//                ProjectDescriptor projectDescriptor =
-//                        ProjectDescriptorsFactory.createProjectDescriptor(file.toURI());
-//                projectsManager.useModule(project, projectDescriptor);
-//                dummyvs = (Package) project.getElementByID("_17_0_2_407019f_1354124289134_280378_12909");
-//            } catch(Exception e){
-//                // didnt work but was worth a try.
-//            }
-//        }
-        if(dummyvs == null){
+        // Auto-mounting proof of concept (untested)
+        /*
+        if (dummyvs == null) {
+            try {
+                ProjectsManager projectsManager = Application.getInstance().getProjectsManager();
+                File file = new File(Application.environment().getProfilesDirectory() + "SysML Extensions.mdxml");
+                ProjectDescriptor projectDescriptor =
+                        ProjectDescriptorsFactory.createProjectDescriptor(file.toURI());
+                projectsManager.useModule(project, projectDescriptor);
+                dummyvs = (Package) project.getElementByID("_17_0_2_407019f_1354124289134_280378_12909");
+            } catch(Exception e){
+                // didnt work but was worth a try.
+            }
+        }
+        */
+        if (dummyvs == null) {
             Application.getInstance().getGUILog().log("You don't have SysML Extensions mounted! You need it in order for the validations to show.");
             return;
         }
