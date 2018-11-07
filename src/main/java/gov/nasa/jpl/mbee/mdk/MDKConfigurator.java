f20d583934df0697c5370485092f2556ffcf9193
DavidWillard <willard@jpl.nasa.gov>
2018-11-07 09:21:29 -0700

    MAGICDRAW-1028 Removed the "Create RestrictedValue" action from the Property Element's DocGen menu. (Existing Restricted Values should still work, but new ones can't be created)

--- src/main/java/gov/nasa/jpl/mbee/mdk/MDKConfigurator.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/MDKConfigurator.java
@@ -229,10 +229,10 @@ public class MDKConfigurator implements BrowserContextAMConfigurator, DiagramCon
         }
 
         // DocGen menu
-        ActionsCategory category = myCategory(manager, "DocGen", "DocGen");
         if (e instanceof Class) {
             Class view = (Class) e;
             if (StereotypesHelper.hasStereotypeOrDerived(view, viewStereotype)) {
+                ActionsCategory category = myCategory(manager, "DocGen", "DocGen");
                 NMAction act = manager.getActionFor(ValidateViewAction.DEFAULT_ID);
                 if (act == null) {
                     category.addAction(new ValidateViewAction(view));
@@ -260,19 +260,22 @@ public class MDKConfigurator implements BrowserContextAMConfigurator, DiagramCon
                 c.addAction(new InstanceViewpointAction(e));
             }
         }
-        ArrayList<Property> selectedProperties = new ArrayList<Property>();
-        for (Element el : es) {
-            if (el instanceof Property) {
-                selectedProperties.add((Property) el);
-            }
-        }
-        if (!(selectedProperties.isEmpty())) {
-            ActionsCategory c = myCategory(manager, "DocGen", "DocGen");
-            NMAction act = manager.getActionFor(CreateRestrictedValueAction.DEFAULT_ID);
-            if (act == null) {
-                c.addAction(new CreateRestrictedValueAction(selectedProperties));
-            }
-        }
+        //Removed restricted value option from DocGen menu to prevent future use while feature is
+        //phased out in favor of enumerations
+//        ArrayList<Property> selectedProperties = new ArrayList<Property>();
+//        for (Element el : es) {
+//            if (el instanceof Property) {
+//                selectedProperties.add((Property) el);
+//            }
+//        }
+//        if (!(selectedProperties.isEmpty())) {
+//            ActionsCategory c = myCategory(manager, "DocGen", "DocGen");
+//            NMAction act = manager.getActionFor(CreateRestrictedValueAction.DEFAULT_ID);
+//            if (act == null) {
+//                c.addAction(new CreateRestrictedValueAction(selectedProperties));
+//            }
+//        }
+
     }
 
     private void addDiagramActions(ActionsManager manager, DiagramPresentationElement diagram) {
