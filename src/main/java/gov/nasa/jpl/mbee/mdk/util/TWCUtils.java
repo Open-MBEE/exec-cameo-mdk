c8742d76589fd40bd4dbc4428ec5d415622ad2b2
ShrayRastogi <shray.rastogi@boeing.com>
2020-06-03 12:34:55 +0530

    Extends ticket processor to acquire mms ticket using twc token

--- src/main/java/gov/nasa/jpl/mbee/mdk/util/TWCUtils.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/util/TWCUtils.java
@@ -1,14 +1,27 @@
 package gov.nasa.jpl.mbee.mdk.util;
 
+import com.nomagic.magicdraw.core.Application;
+import com.nomagic.magicdraw.core.Project;
 import com.nomagic.magicdraw.esi.EsiUtils;
+import com.nomagic.ui.ProgressStatusRunner;
 import com.nomagic.magicdraw.teamwork2.esi.EsiServerActionsExecuter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.ConnectException;
+import gov.nasa.jpl.mbee.mdk.mms.MMSUtils;
+import gov.nasa.jpl.mbee.mdk.util.TicketUtils.TicketMapping;
 
-public class TWCUtils {
+import java.net.ConnectException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import gov.nasa.jpl.mbee.mdk.http.ServerException;
+
+public class TWCUtils extends AcquireTicketProcessor {
+    public TWCUtils(AcquireTicketProcessor processor) {
+        super(processor);
+    }
+
     private static final Logger logger = LoggerFactory.getLogger(TWCUtils.class);
 
     public static String getConnectedUser() {
@@ -31,5 +44,34 @@ public class TWCUtils {
             logger.error("Error while generating secondary auth token", e.getMessage());
         }
         return secondaryAuthToken != null ? "Token :" + secondaryAuthToken : null;
+    }
+
+    @Override
+    public boolean acquireMmsTicket(Project project) {
+        if (getConnectedUser() == null || getTeamworkCloudServer() == null || getSecondaryAuthToken() == null) {
+            return super.acquireMmsTicket(project);
+        }
+
+        // do request
+        ProgressStatusRunner.runWithProgressStatus(progressStatus -> {
+            String ticket;
+            try {
+                ticket = MMSUtils.getTicketUsingTWCToken(project, getTeamworkCloudServer(), getSecondaryAuthToken(),
+                        progressStatus);
+            } catch (IOException | URISyntaxException | ServerException e) {
+                Application.getInstance().getGUILog()
+                        .log("[ERROR] An error occurred while acquiring credentials. Reason: " + e.getMessage());
+                e.printStackTrace();
+                return;
+            }
+            if (ticket != null) {
+                TicketUtils.ticketMappings.put(project, new TicketMapping(project, getConnectedUser(), ticket));
+            }
+        }, "Logging in to MMS", true, 0);
+        if (TicketUtils.isTicketSet(project)) {
+            return true;
+        }
+        Application.getInstance().getGUILog().log("[ERROR] Unable to log in to MMS with TWC.");
+        return super.acquireMmsTicket(project);
     }
 }
