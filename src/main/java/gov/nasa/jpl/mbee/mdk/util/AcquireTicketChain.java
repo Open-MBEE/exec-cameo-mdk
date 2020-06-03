9a4a7cc1acd65b8e7285ee1d0f204aa59c9d51dc
ShrayRastogi <shray.rastogi@boeing.com>
2020-06-03 12:33:43 +0530

    Builds acquire mms ticket chain

---
+++ src/main/java/gov/nasa/jpl/mbee/mdk/util/AcquireTicketChain.java
@@ -0,0 +1,19 @@
+package gov.nasa.jpl.mbee.mdk.util;
+
+import com.nomagic.magicdraw.core.Project;
+
+public class AcquireTicketChain {
+    AcquireTicketProcessor chain;
+
+    public AcquireTicketChain() {
+        buildChain();
+    }
+
+    private void buildChain() {
+        chain = new TWCUtils(new TicketUtils(null));
+    }
+
+    public boolean acquireMmsTicket(Project project) {
+        return chain.acquireMmsTicket(project);
+    }
+}
