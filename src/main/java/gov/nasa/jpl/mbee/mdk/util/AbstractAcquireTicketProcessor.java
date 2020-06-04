cd97b8921ef7c06ca8f05b2a3b11c01918f20062
ShrayRastogi <shray.rastogi@boeing.com>
2020-06-04 17:29:30 +0530

    Renames class to AbstractAcquireTicketProcessor

---
+++ src/main/java/gov/nasa/jpl/mbee/mdk/util/AbstractAcquireTicketProcessor.java
@@ -0,0 +1,25 @@
+package gov.nasa.jpl.mbee.mdk.util;
+
+import com.nomagic.magicdraw.core.Application;
+import com.nomagic.magicdraw.core.Project;
+
+import gov.nasa.jpl.mbee.mdk.mms.MMSUtils;
+
+public abstract class AbstractAcquireTicketProcessor {
+    private AbstractAcquireTicketProcessor processor;
+
+    public AbstractAcquireTicketProcessor(AbstractAcquireTicketProcessor processor) {
+        this.processor = processor;
+    }
+
+    public boolean acquireMmsTicket(Project project) {
+        if (MMSUtils.getServerUrl(project) == null) {
+            Application.getInstance().getGUILog().log("[ERROR] MMS url is not specified. Skipping login.");
+            return false;
+        }
+        if (processor != null) {
+            return processor.acquireMmsTicket(project);
+        }
+        return false;
+    }
+}
