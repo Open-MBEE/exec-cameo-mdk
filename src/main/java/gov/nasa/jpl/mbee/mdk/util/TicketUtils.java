39c6281d8e4e8cfb4dfc03ced62f1ac5cdb577a9
ShrayRastogi <shray.rastogi@boeing.com>
2020-06-15 16:31:49 +0530

    Validate jwt token checks against logged in Twc user

--- src/main/java/gov/nasa/jpl/mbee/mdk/util/TicketUtils.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/util/TicketUtils.java
@@ -55,7 +55,7 @@ public class TicketUtils extends AbstractAcquireTicketProcessor{
         if (!isTicketSet(project)) {
             return false;
         }
-        if(MMSUtils.validateJwtToken(project, progressStatus)) {
+        if(TWCUtils.getConnectedUser() != null && MMSUtils.validateJwtToken(project, progressStatus).equalsIgnoreCase(TWCUtils.getConnectedUser())) {
             return true;
         }
         return MMSUtils.validateCredentialsTicket(project, getTicket(project), progressStatus).equals(username);
