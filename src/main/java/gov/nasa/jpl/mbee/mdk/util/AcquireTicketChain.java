43f5e5d56a48ff7913774f4c86e04d3df97d2b34
ShrayRastogi <shray.rastogi@boeing.com>
2020-06-30 14:03:30 +0530

    Dynamically builds auth chain based on order provided in properties

--- src/main/java/gov/nasa/jpl/mbee/mdk/util/AcquireTicketChain.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/util/AcquireTicketChain.java
@@ -1,17 +1,43 @@
 package gov.nasa.jpl.mbee.mdk.util;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 import com.nomagic.magicdraw.core.Project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gov.nasa.jpl.mbee.mdk.options.MDKOptionsGroup;
 
 public class AcquireTicketChain {
     AbstractAcquireTicketProcessor chain;
+    private static final Logger logger = LoggerFactory.getLogger(AcquireTicketChain.class);
 
     public AcquireTicketChain() {
         buildChain();
     }
 
-    // TODO: to add the OAuth implementation as the third in chain
+    private AbstractAcquireTicketProcessor recursiveBuildChain(List<String> authChain) throws NoSuchMethodException,
+            ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        if (authChain.size() > 1) {
+            Constructor c = Class.forName(authChain.get(0)).getConstructor(AbstractAcquireTicketProcessor.class);
+            authChain.remove(0);
+            return (AbstractAcquireTicketProcessor) c.newInstance(recursiveBuildChain(authChain));
+        } else {
+            return new AuthenticationChainError(null);
+        }
+    }
+
     private void buildChain() {
-        chain = new TWCUtils(new TicketUtils(null));
+        List<String> authChain = MDKOptionsGroup.getMDKOptions().getAuthenticationChain();
+        try {
+            chain = recursiveBuildChain(authChain);
+        } catch (NoSuchMethodException | SecurityException | ClassNotFoundException | InstantiationException
+                | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            logger.error("Error in creating authentication chain" + e.getMessage());
+        }
     }
 
     public boolean acquireMmsTicket(Project project) {
