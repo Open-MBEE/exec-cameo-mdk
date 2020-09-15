7f81753b76f8e239c2406561836cafc6b9afeea4
ShrayRastogi <shray.rastogi@boeing.com>
2020-09-15 12:58:04 +0530

    Adds twc server url to project json. Shows confirm dialog if associated twc service changed.

--- src/main/java/gov/nasa/jpl/mbee/mdk/mms/validation/ProjectValidator.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/mms/validation/ProjectValidator.java
@@ -21,6 +21,7 @@ import gov.nasa.jpl.mbee.mdk.validation.ValidationSuite;
 import gov.nasa.jpl.mbee.mdk.validation.ViolationSeverity;
 import org.apache.http.client.methods.HttpRequestBase;
 
+import javax.swing.JOptionPane;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -52,10 +53,31 @@ public class ProjectValidator {
     private boolean errors;
     private ValidationSuite validationSuite = new ValidationSuite("structure");
     private ValidationRule projectExistenceValidationRule = new ValidationRule("Project Existence", "The project shall exist in the specified site.", ViolationSeverity.ERROR);
+    private static boolean isTWCServiceChanged = false;
 
     public ProjectValidator(Project project) {
         this.project = project;
         validationSuite.addValidationRule(projectExistenceValidationRule);
+    }
+
+    public static String getTeamworkCloudServer() {
+        String twcServer = null;
+        if (EsiUtils.getTeamworkService() != null && EsiUtils.getTeamworkService().getLastUsedLoginInfo() != null
+                && EsiUtils.getTeamworkService().getLastUsedLoginInfo().server != null) {
+            twcServer = EsiUtils.getTeamworkService().getLastUsedLoginInfo().server;
+            if (twcServer.indexOf(':') > -1) {
+                twcServer = twcServer.substring(0, twcServer.indexOf(':'));
+            }
+        }
+        return twcServer;
+    }
+
+    public static void setTWCServiceChanged(JsonNode projectJson) {
+        JsonNode value;
+        if((value = projectJson.get(MDKConstants.TWC_HEADER)) != null && value.isTextual() && !value.asText().isEmpty()) {                        
+            String twcServerUrl = getTeamworkCloudServer();
+            isTWCServiceChanged = (twcServerUrl != null) ? twcServerUrl.equalsIgnoreCase(value.asText()) : isTWCServiceChanged;
+        }
     }
 
     public void validate() {
@@ -78,6 +100,7 @@ public class ProjectValidator {
             for (JsonNode projectJson : projectsJson) {
                 if ((value = projectJson.get(MDKConstants.ID_KEY)) != null && value.isTextual()
                         && value.asText().equals(Converters.getIProjectToIdConverter().apply(project.getPrimaryProject()))) {
+                    setTWCServiceChanged(projectJson);
                     return;
                 }
             }
@@ -105,8 +128,21 @@ public class ProjectValidator {
         projectObjectNode.put(MDKConstants.ORG_ID_KEY, orgId);
         projectObjectNode.put(MDKConstants.PROJECT_TYPE_KEY, MDKConstants.PROJECT_TYPE_VALUE);
         String resourceId = "";
+        String twcServerUrl = "";
         if (ProjectUtilities.getProject(iProject).isRemote()) {
             resourceId = ProjectUtilities.getResourceID(iProject.getLocationURI());
+            twcServerUrl = getTeamworkCloudServer();
+        }
+        if(twcServerUrl != null && !twcServerUrl.isEmpty()) {
+            if(isTWCServiceChanged) {
+                int response = JOptionPane.showConfirmDialog(Application.getInstance().getMainFrame(), "Would you like to associate this project with current TWC Service?",
+                        "Associated TWC Service Changed", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+                if (response == JOptionPane.OK_OPTION) {
+                    projectObjectNode.put(MDKConstants.TWC_HEADER, twcServerUrl);
+                }
+            } else {
+                projectObjectNode.put(MDKConstants.TWC_HEADER, twcServerUrl);
+            }
         }
         projectObjectNode.put(MDKConstants.TWC_ID_KEY, resourceId);
         String categoryId = "";
