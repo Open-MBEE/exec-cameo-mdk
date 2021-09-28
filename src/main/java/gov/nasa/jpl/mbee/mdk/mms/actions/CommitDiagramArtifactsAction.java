9508e3793c46654e10ea1b8915d35ec1ee17d609
Enquier <enquier@users.noreply.github.com>
2021-09-28 16:31:52 -0600

    Artifact deployment ready for merge

--- src/main/java/gov/nasa/jpl/mbee/mdk/mms/actions/CommitDiagramArtifactsAction.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/mms/actions/CommitDiagramArtifactsAction.java
@@ -68,29 +68,14 @@ public class CommitDiagramArtifactsAction extends RuleViolationAction implements
                 String refId = MDUtils.getBranchId(project);
                 for (MMSArtifact artifact : artifacts) {
                     HttpEntity entity = MultipartEntityBuilder.create()
-//                            .addTextBody(MDKConstants.ID_KEY, artifact.getId())
-//                            .addTextBody(MDKConstants.CHECKSUM_KEY, artifact.getChecksum())
-//                            .addTextBody("source", "magicdraw")
-                            .addBinaryBody("file", artifact.getFile(), artifact.getContentType(), artifact.getId() + "_" + artifact.getExtension() + ".tmp").build();
+                            .addTextBody("source", "magicdraw")
+                            .addBinaryBody("file", artifact.getFile(), artifact.getContentType(), artifact.getId() + "." + artifact.getExtension()).build();
                     HttpRequestBase artifactRequest = MMSUtils.prepareEndpointBuilderMultipartPostRequest(MMSElementEndpoint.builder(), project, entity)
                             .addParam(MMSEndpointBuilderConstants.URI_PROJECT_SUFFIX, projectId)
                             .addParam(MMSEndpointBuilderConstants.URI_REF_SUFFIX, refId)
                             .addParam(MMSEndpointBuilderConstants.URI_ELEMENT_SUFFIX, artifact.getId()).build();
                     MMSUtils.sendMMSRequest(project, artifactRequest, progressStatus);
-                    //if(!file.delete()) { // if we cannot immediately delete we'll get it later
-                    //    file.deleteOnExit();
-                    //}
                 }
-//                ObjectNode objectNode = JacksonUtils.getObjectMapper().createObjectNode();
-//                objectNode.put(MDKConstants.ID_KEY, Converters.getElementToIdConverter().apply(diagram));
-//                ArrayNode updatedArtifactIdsNode = objectNode.putArray(MDKConstants.ARTIFACT_IDS_KEY);
-//                initialArtifactIds.forEach(updatedArtifactIdsNode::add);
-//                artifacts.stream().map(MMSArtifact::getId).forEachOrdered(updatedArtifactIdsNode::add);
-//                File file = MMSUtils.createEntityFile(CommitClientElementAction.class, ContentType.APPLICATION_JSON, Collections.singleton(objectNode), MMSUtils.JsonBlobType.ELEMENT_JSON);
-//                HttpRequestBase elementPostRequest = MMSUtils.prepareEndpointBuilderBasicJsonPostRequest(MMSElementsEndpoint.builder(), project, file)
-//                        .addParam(MMSEndpointBuilderConstants.URI_PROJECT_SUFFIX, projectId)
-//                        .addParam(MMSEndpointBuilderConstants.URI_REF_SUFFIX, refId).build();
-//                MMSUtils.sendMMSRequest(project, elementPostRequest, progressStatus);
             } catch (IOException | ServerException | URISyntaxException | GeneralSecurityException e) {
                 e.printStackTrace();
                 Application.getInstance().getGUILog().log("[ERROR] Failed to commit diagram artifacts for " + Converters.getElementToHumanNameConverter().apply(diagram) + ". Reason: " + e.getMessage());
@@ -102,68 +87,4 @@ public class CommitDiagramArtifactsAction extends RuleViolationAction implements
     public boolean canExecute(Collection<Annotation> collection) {
         return true;
     }
-
-    /*
-        ObjectNode imageEntry = JacksonUtils.getObjectMapper().createObjectNode();
-        //for (Element e: Project.getProject(image.getImage()).getDiagram(image.getImage()).getUsedModelElements(false)) {
-        //    addToElements(e);
-        //}
-        // export image - also keep track of exported images
-        DiagramPresentationElement diagram = Application.getInstance().getProject().getDiagram(image.getImage());
-        String svgFilename = Converters.getElementToIdConverter().apply(image.getImage());
-
-        // create image file
-        String userhome = System.getProperty("user.home");
-        File directory;
-        if (userhome != null) {
-            directory = new File(userhome + File.separator + "mdkimages");
-        }
-        else {
-            directory = new File("mdkimages");
-        }
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-
-        // export the image file
-        File svgDiagramFile = new File(directory, svgFilename);
-        boolean initialUseSVGTestTag = Application.getInstance().getEnvironmentOptions().getGeneralOptions().isUseSVGTextTag();
-        Application.getInstance().getEnvironmentOptions().getGeneralOptions().setUseSVGTextTag(true);
-        try {
-            ImageExporter.export(diagram, ImageExporter.SVG, svgDiagramFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            Application.getInstance().getEnvironmentOptions().getGeneralOptions().setUseSVGTextTag(initialUseSVGTestTag);
-        }
-
-        // calculate the checksum
-        long cs = 0;
-        try {
-            RandomAccessFile f = new RandomAccessFile(svgDiagramFile.getAbsolutePath(), "r");
-            byte[] data = new byte[(int) f.length()];
-            f.read(data);
-            f.close();
-            Checksum checksum = new CRC32();
-            checksum.update(data, 0, data.length);
-            cs = checksum.getValue();
-        } catch (IOException e) {
-            gl.log("Could not calculate checksum: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        // Lets rename the file to have the hash code
-        // make sure this matches what's in the View Editor ImageResource.java
-        String FILE_EXTENSION = "svg";
-        //gl.log("Exporting diagram to: " + svgDiagramFile.getAbsolutePath());
-
-        // keep record of all images found
-        imageEntry.put("cs", String.valueOf(cs));
-        imageEntry.put("abspath", svgDiagramFile.getAbsolutePath());
-        imageEntry.put("extension", FILE_EXTENSION);
-        images.put(svgFilename, imageEntry);
-
-        //MDEV #674 -- Update the type and id: was hard coded.
-        //
-     */
 }
